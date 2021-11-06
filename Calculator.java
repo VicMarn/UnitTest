f73284bb33affabafd755fa52bdedfd207fd92fe
VicMarn <vctrmonteiro@hotmail.com>
2021-11-06 12:11:52 -0300

    Calculator's methods implemented

--- Calculator.java
+++ Calculator.java
@@ -3,19 +3,19 @@ package CalculatorTest;
 public class Calculator {
 
     public static int add(int x, int y){
-        return 0;
+        return x+y;
     }
 
     public static int sub(int x, int y){
-        return 0;
+        return x-y;
     }
 
     public static int mult(int x,int y){
-        return 0;
+        return x*y;
     }
 
     public static int div(int x,int y){
-        return 0;
+        return x/y;
     }
 
 }
